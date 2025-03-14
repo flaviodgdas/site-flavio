9993ff88cc7dd973b8ce7026458f79a7c1f74846
Flávio Domingos Gomes da Silva <161535510+flaviodgdas@users.noreply.github.com>
2025-03-14 10:37:41 -0300

    atualizando

---
+++ desenhos.js
@@ -0,0 +1,6 @@
+//Algumas perguntas e depois abre a página.
+window.alert('É muito importante você responder as perguntas seguintes')
+        window.confirm('Estar gostando deste site?')
+        window.prompt('Qual é o seu nome?')
+        window.alert('Obrigado pela colaboração, bem vindo a galeria')
+        
